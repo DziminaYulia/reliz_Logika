32a1f0a2afd48b4d9b178b2c0bcafee3709870ca
Julia <dziminalogika@gmail.com>
2025-11-01 13:24:41 +0200

    day 3

--- main.js
+++ main.js
@@ -14,9 +14,52 @@ function getCardHTML(product) {
   <div class="card-body">
     <h5 class="card-title">${product.title}</h5>
     <p class="card-text">${product.price}</p>
-    <a href="#" class="btn btn-primary">Додати в кошик</a>
+    <a href="#" class="btn btn-primary" data-product='${JSON.stringify(
+      product
+    )}'>Додати в кошик</a>
   </div>
 </div>`;
+}
+
+class ShoppingCart {
+  constructor() {
+    this.items = {}; // об’єкт з товарами у кошику
+    this.total = 0; // загальна вартість замовлення
+  }
+
+  addItem(item) {
+    // Додавання товару до кошика
+    if (this.items[item.title]) {
+      this.items[item.title].quantity += 1;
+    } else {
+      this.items[item.title] = item;
+      this.items[item.title].quantity = 1;
+    }
+    this.saveCartToCookies();
+  }
+
+  saveCartToCookies() {
+    // збереження кошика у кукі
+    let cartJSON = JSON.stringify(this.items);
+    document.cookie = `cart=${cartJSON}; max-age=${60 * 60 * 24 * 7}; path=/`;
+  }
+
+  loadCartFromCookies() {
+    // Завантаження кошика з кукі
+    let cartCookie = getCookieValue("cart");
+    if (cartCookie && cartCookie !== "") {
+      this.items = JSON.parse(cartCookie);
+    }
+  }
+}
+let cart = new ShoppingCart(); // Створення об'єкта кошика
+
+function addToCart(event) {
+  // Отримуємо дані про товар з data-атрибута кнопки
+  const productData = event.target.getAttribute("data-product");
+  const product = JSON.parse(productData); // перетворюємо JSON у об’єкт
+  // Тут будемо додавати товар до кошика
+  cart.addItem(product);
 }
 
 // Викликаємо асинхронну функцію та очікуємо на отримання продуктів
